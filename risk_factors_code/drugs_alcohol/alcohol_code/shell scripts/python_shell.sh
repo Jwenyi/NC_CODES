a967a94f63602703d4b58f37445834f46a92de1f
Jwenyi <71015117+Jwenyi@users.noreply.github.com>
2025-03-21 21:43:28 +0800

    Delete risk_factors_code directory

--- risk_factors_code/drugs_alcohol/alcohol_code/shell scripts/python_shell.sh
+++
@@ -1,7 +0,0 @@
-
-echo "$@"
-
-#Activate custom python environment with shared functions
-source  gbd_env
-
-python "$@"
--- risk_factors_code/metab_cholesterol/covariate_selection_code/shell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#$ -S /bin/sh
-FILEPATH/bin/R < FILEPATH/linear_oos_process.R --no-save --args $1 $2 $3 $4 $5 $6 $7

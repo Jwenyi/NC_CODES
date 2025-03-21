27b308b8b91ddf3a20b29ff91470362e40018271
Jwenyi <71015117+Jwenyi@users.noreply.github.com>
2025-03-21 21:42:27 +0800

    Delete cod_code directory

--- cod_code/ntd_echino/submit_prediction_processor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-#$ -S FILEPATH/stata-mp -q FILEPATH/prediction_processor.do "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
--- cod_code/ntd_zika/05c_submit_death_processing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#$ -S /bin/sh
-/usr/local/bin/stata-mp -q FILEPATH/05d_death_processing.do "$1"  
